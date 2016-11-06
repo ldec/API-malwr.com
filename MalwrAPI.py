d6ac7c3f8ae584fbeb1cec14a29b7a4967cb12e1
Louis Declerfayt <louis@declerfayt.com>
2016-11-06 15:31:49 +0100

    Added cookeir export

--- MalwrAPI.py
+++ MalwrAPI.py
@@ -1,21 +1,10 @@
-"""
-This is the (unofficial) Python API for malwr.com Website.
-Using this code, you can retrieve recent analyses, domains, tags but also latest comments.
-You can also submit files
-
-"""
 import hashlib
-
 import re
 import requests
-import os
 from bs4 import BeautifulSoup
 
-
 class MalwrAPI(object):
-    """
-        MalwrAPI Main Handler
-    """
+    global response
     session = None
     logged = False
     verbose = False
@@ -25,12 +14,13 @@ class MalwrAPI(object):
                       "Gecko/20100101 Firefox/41.0"
     }
 
-    def __init__(self, verbose=False, username=None, password=None, apikey=None):
+    def __init__(self, verbose=False, username=None, password=None, apikey=None, cookie=None):
         self.verbose = verbose
         self.session = requests.session()
         self.username = username
         self.password = password
         self.apikey = apikey
+        self.cookie=cookie
 
     def login(self):
         """Login on malwr.com website"""
@@ -46,6 +36,10 @@ class MalwrAPI(object):
             login_request = self.session.post(self.url + "/account/login/",
                                               data=payload, headers=self.headers)
 
+            response = self.session.get(self.url + "/account/login/",
+                                              data=payload, headers=self.headers)
+
+            self.cookie=response.cookies["sessionid"]
             if login_request.status_code == 200:
                 self.logged = True
                 return True
@@ -57,7 +51,6 @@ class MalwrAPI(object):
         """Request url and return the Beautifoul Soup object of html returned"""
         if not url:
             url = self.url
-
         req = self.session.get(url, headers=self.headers)
         soup = BeautifulSoup(req.content, "html.parser")
         return soup
@@ -98,7 +91,6 @@ class MalwrAPI(object):
                 'url_analysis': infos[1].find('a')['href']
             }
             res.append(infos_to_add)
-
         return res
 
     def get_public_tags(self):
@@ -110,7 +102,6 @@ class MalwrAPI(object):
         tags = soup.findAll('div', {'class': 'span6'})[2]
         for tag in tags.findAll('a', {'class': 'tag-label'}):
             res.append(tag.string)
-
         return res
 
     def get_recent_analyses(self):
@@ -121,21 +112,13 @@ class MalwrAPI(object):
         submissions = soup.findAll('div', {'class': 'span6'})[0]
         for submission in submissions.findAll('tr'):
             infos = submission.findAll('td')
-
             infos_to_add = {
                 'submission_time': infos[0].string,
                 'hash': infos[1].find('a').string,
                 'submission_url': infos[1].find('a')['href']
+
             }
             res.append(infos_to_add)
-
-        return res
-
-    def submit_folder(self, path, analyze=True, share=True, private=True):
-        filelist = [f for f in os.listdir(path)]
-        res = []
-        for item in filelist:
-            res.append(self.submit_sample(path + item, analyze, share, private))
         return res
 
     def submit_sample(self, filepath, analyze=True, share=True, private=True):
@@ -144,12 +127,9 @@ class MalwrAPI(object):
 
         s = self.session
         req = s.get(self.url + '/submission/', headers=self.headers)
-
         soup = BeautifulSoup(req.content, "html.parser")
-
         # TODO: math_captcha_question might be unused. Remove.
         # math_captcha_question = soup.find('input', {'name': 'math_captcha_question'})['value']
-
         pattern = '(\d [-+*] \d) ='
         data = {
             'math_captcha_field': eval(re.findall(pattern, req.content)[0]),
@@ -165,7 +145,6 @@ class MalwrAPI(object):
 
         # TODO: soup might be unused. Remove.
         # soup = BeautifulSoup(req.content, "html.parser")
-
         # regex to check if the file was already submitted before
         pattern = '(\/analysis\/[a-zA-Z0-9]{12,}\/)'
         submission_links = re.findall(pattern, req.content)
@@ -227,43 +206,10 @@ class MalwrAPI(object):
                 'submission_time': infos[0].string,
                 'hash': infos[1].find('a').string,
                 'submission_url': infos[1].find('a')['href'],
-                'file_name': infos[2].string
+                'file_name': infos[2].string,
+                'cookie': self.cookie
             }
             res.append(infos_to_add)
 
         return res
 
-    def getReport(self, search_url):
-        # Do nothing if not logged in
-        if not self.logged:
-            res = self.login()
-            if res is False:
-                return False
-
-        search_url = self.url + search_url
-        c = self.request_to_soup(search_url)
-
-        csrf_input = c.find(attrs=dict(name='csrfmiddlewaretoken'))
-        csrf_token = csrf_input['value']
-        payload = {
-            'csrfmiddlewaretoken': csrf_token,
-        }
-        sc = self.session.post(search_url, data=payload, headers=self.headers)
-        ssc = BeautifulSoup(sc.content, "html.parser")
-        
-        output = {"IP": [], "Domain": []}
-       
-        domains = ssc.find(id="domains").find_all("td")
-        # Will go domain, IP, domain, IP
-        for i in range(len(domains)):
-            if i%2 == 0:
-                # Domain
-                output["Domain"].append(domains[i].text)
-            else:
-                # IP 
-                output["IP"].append(domains[i].text)
-    
-        ips = ssc.find(id="hosts").find_all("td")
-        output["IP"] += [x.text for x in ips]
-        
-        return output
